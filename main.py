daa468b34946c81ac7bfd3051cff5750d5af9e48
Panzerschwein <47000673+Panzerschwein@users.noreply.github.com>
2021-03-25 10:22:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gspread
-from webScraper import decodeCSV, decodeDescription
-from spreadsheetUpdate import updateSheet, updateTreePurge
+from webScraper import decodeCSV
+from spreadsheetUpdate import updateSheet
 specificWorksheet = input("""What is the index of the worksheet that you want to write the data to?
 (0 if its the first sheet on the spreadsheet, 1 if its the second and so on)
 > """)
@@ -8,5 +8,3 @@ csvFile = input("""What is the name of the csv file that you want to read? (ex. 
 > """)
 count, names = decodeCSV(csvFile)
 updateSheet(count, specificWorksheet)
-x = decodeDescription('roy_description.csv')
-updateTreePurge(x)
