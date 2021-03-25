e69ded67bd0a297a44c6b243fadf84da3935b10e
Panzerschwein <47000673+Panzerschwein@users.noreply.github.com>
2021-03-25 10:22:39 -0500

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -18,26 +18,3 @@ def decodeCSV(csvFile):
         else:
             uniqueCount[row[0]] = (1, row[1])
     return uniqueCount, normalName
-
-def decodeDescription(csvFile):
-    uniqueCount = {}
-    f = open(csvFile, encoding='utf-8')
-    csv_f = csv.reader(f)
-    for row in csv_f:
-        print(row)
-        if row[1] in uniqueCount:
-            tuple1 = uniqueCount[row[1]]
-            x = tuple1[0]
-            y = tuple1[1]
-            z = tuple1[2]
-            y = y+1
-            a = (x,y,z)
-            uniqueCount[row[1]] = a
-        else:
-            uniqueCount[row[1]] = (row[0], 1, row[2])
-        for i in uniqueCount:
-            if 'tree' in uniqueCount[i][0] or 'shrub' in uniqueCount[i][0]:
-                print(True)
-            else:
-                print(False)
-    return uniqueCount
