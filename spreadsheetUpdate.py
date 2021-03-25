c5ab5a3e61c2c69388e3a474704cf7032cf4ec4d
Panzerschwein <47000673+Panzerschwein@users.noreply.github.com>
2021-03-25 09:37:20 -0500

    Update spreadsheetUpdate.py

--- spreadsheetUpdate.py
+++ spreadsheetUpdate.py
@@ -14,20 +14,4 @@ def updateSheet(speciesCount, parkName):
     placing = str('A6:C' + str(count))
     parkSheet.update(placing, countList)
 
-def updateTreePurge(speciesCount):
-    count = 6
-    countList = []
-    parkSheet = sh.get_worksheet(2)
-
-
-    for i in speciesCount:
-        if 'tree' in speciesCount[i][0] or 'shrub' in speciesCount[i][0]:
-            countNameTuple = [speciesCount[i][2], i, speciesCount[i][1]]
-            countList.append(countNameTuple)
-        else:
-            print(False)
-        count = count +1
-    placing = str('A6:C' + str(count))
-    parkSheet.update(placing, countList)
-
 
