79de83931cceb6677b9f842388905e8c3603ff8d
Panzerschwein <47000673+Panzerschwein@users.noreply.github.com>
2021-03-26 10:28:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import gspread
 from webScraper import decodeCSV
 from spreadsheetUpdate import updateSheet
-specificWorksheet = input("""What is the index of the worksheet that you want to write the data to?
+specificWorksheet = int(input("""What is the index of the worksheet that you want to write the data to?
 (0 if its the first sheet on the spreadsheet, 1 if its the second and so on)
-> """)
+> """))
 csvFile = input("""What is the name of the csv file that you want to read? (ex. bob.csv)
 > """)
 count, names = decodeCSV(csvFile)
