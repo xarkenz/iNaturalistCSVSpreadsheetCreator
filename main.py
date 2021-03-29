8ad9ff5dacae5c8b10fde8b2b16ee50b3cdb547b
Panzerschwein <47000673+Panzerschwein@users.noreply.github.com>
2021-03-29 08:05:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
-import gspread
 from webScraper import decodeCSV, crossReference
 from spreadsheetUpdate import updateSheet
 specificWorksheet = int(input("""What is the index of the worksheet that you want to write the data to?
-(0 if its the first sheet on the spreadsheet, 1 if its the second and so on)
+(0 if its the first sheet on the spreadsheet, 1 if its the second and so on) Put 0 if you haven't created any more worksheets in your Google Sheet
 > """))
 csvFile = input("""What is the name of the csv file that you want to read? (ex. bob.csv)
 > """)
 count, names = decodeCSV(csvFile)
-count = crossReference(count)
+enable = input("""Would you like to enable Sorting Mode? (y/n) Sorting mode will automatically eliminate plants that are not woody plants, and leave you with a smaller dataset to go through.
+> """)
+if enable.lower() == 'y' or enable.lower() == 'yes':
+	count = crossReference(count)
+
 updateSheet(count, specificWorksheet)
