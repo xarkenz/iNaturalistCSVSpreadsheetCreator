89de48af948e887c8d5081750d9f76c821817546
Sean Clarke <46010001+xarkenz@users.noreply.github.com>
2021-05-13 00:52:51 -0500

    Update SheetManager.py to separator version

--- SheetManager.py
+++ SheetManager.py
@@ -1,26 +1,60 @@
 import csv
+from os import mkdir
 
-def read_csv(read_path:str):
+def read_csv(read_path: str):
     # Import and group the CSV file
     data = {}
+    common_names = {}
     with open(read_path, "r") as f:
         read_file = csv.reader(f)
         for row in read_file:
-            if row[0] == "scientific_name":
-                # Ignore the header row, we will add it back in the export step
-                continue
-            elif row[0] in data:
-                # If we already have the species, we can just increment the number
-                data[row[0]][0] += 1
-            else:
-                # Create an entry for the species if it doesn't exist
-                data[row[0]] = [1, row[1]]
-        return data
+            if len(row) == 2:
+                if row[0].lower() in ["scientific_name", "scientific name"]:
+                    if row[1].lower() != "count":
+                        raise ValueError("CSV columns are improperly formatted. See README.md for instructions.")
+                    # Ignore the header row, we will add it back in the export step
+                    continue
+                elif row[0] in data:
+                    # If we already have the species, we can just combine
+                    data[row[0]] += int(row[1])
+                else:
+                    # Create an entry for the species if it doesn't exist
+                    data[row[0]] = int(row[1])
+            elif len(row) == 3:
+                if row[1] in ["scientific_name", "Scientific Name"]:
+                    if row[2].lower() != "count" or row[0].lower() not in ["common_name", "common name"]:
+                        raise ValueError("CSV columns are improperly formatted. See README.md for instructions.")
+                    # Ignore the header row, we will add it back in the export step
+                    continue
+                elif row[1] in data:
+                    # If we already have the species, we can just combine
+                    data[row[1]] += int(row[2])
+                else:
+                    # Create an entry for the species if it doesn't exist
+                    data[row[1]] = int(row[2])
+                    common_names[row[1]] = row[0]
+        return (data, common_names)
 
-def export_csv(write_path:str, csv_data:dict):
-    # Export the filtered CSV file
-    with open(write_path, "w") as f:
-        write_file = csv.writer(f, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL) # Options might not be necessary but it can't hurt
-        write_file.writerow(["Common Name", "Scientific Name", "Count"]) # Insert a more descriptive header row since we removed it while grouping
-        for species in csv_data.keys():
-            write_file.writerow([csv_data[species][1], species, str(csv_data[species][0])]) # Line order: common_name, scientific_name, count
+def export_csv(write_path: str, csv_data: list, common_names: dict = {}):
+    # Export the filtered CSV files
+    if write_path.endswith("/"):
+        write_path = write_path[:-1]
+    
+    try:
+        mkdir(write_path)
+    except FileExistsError:
+        # User would have already been warned of this fact
+        pass
+    
+    filenames = ["trees.csv", "shrubs.csv", "vines.csv", "unknown.csv"]
+    for i in range(len(filenames)):
+        with open(write_path + "/" + filenames[i], "w") as f:
+            write_file = csv.writer(f, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+            if len(common_names) > 0:
+                write_file.writerow(["Common Name", "Scientific Name", "Count"])
+                for species in csv_data[i].keys():
+                    write_file.writerow([common_names[species], species, str(csv_data[i][species])])  # Line order: common_name, scientific_name, count
+            else:
+                write_file.writerow(["Scientific Name", "Count"])
+                for species in csv_data[i].keys():
+                    write_file.writerow([species, str(csv_data[i][species])])  # Line order: scientific_name, count
